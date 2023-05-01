460b49f1e44cfdffa3f56f0622e6436ab8b0b648
Alyssa Red <redalyssa636@gmail.com>
2023-05-01 18:55:31 -0400

    back end schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,62 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        user: async (parent, {username}) => {
+            return User.findOne({username});
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('Sorry, no user with this ID!');
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError("Login Resolver: Can't find this user");
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError("Wrong password!");
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+          saveBook: async (parent, {book}, context) => {
+            if (context.user) {
+                return (updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {savedBooks: book}},
+                    {new: true, runValidators: true}
+                ));
+            }
+            throw new AuthenticationError("Sorry! You're not logged in!");
+          },
+          removeBook: async (parent, {bookId}, context) => {
+            if (context.user) {
+                return (updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId: bookId}}},
+                    {new: true}
+                ));
+            }
+            throw new AuthenticationError("Sorry! You're not logged in!")
+          }
+    }
+}
